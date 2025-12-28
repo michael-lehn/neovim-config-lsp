06284ec85633f0a03c9d8b755cb0b45917d0af61
Michael Lehn <michael.lehn@uni-ulm.de>
2025-12-28 23:41:39 +0100

    clangd support

--- lua/user/lsp/mason.lua
+++ lua/user/lsp/mason.lua
@@ -8,13 +8,29 @@ local handlers = require('user.lsp.handlers')
 -- ---------------------------------------------------------------------
 require('mason').setup()
 
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        'lua_ls',
-        'stylua',
-    },
-    automatic_installation = true,
-})
+local mlsp = require('mason-lspconfig')
+local registry = require('mason-registry')
+
+local function want_mason_pkg(pkg_name, bin_name)
+    if vim.fn.executable(bin_name) == 1 then
+        return false
+    end
+    return registry.has_package(pkg_name)
+end
+
+local ensure = { 'lua_ls' }  -- LSP
+
+if want_mason_pkg('stylua', 'stylua') then
+    table.insert(ensure, 'stylua')
+end
+
+if want_mason_pkg('clang-format', 'clang-format') then
+    table.insert(ensure, 'clang-format')
+end
+
+if want_mason_pkg('clangd', 'clangd') then
+    table.insert(ensure, 'clangd')
+end
 
 -- ---------------------------------------------------------------------
 -- Global defaults for all LSP servers
