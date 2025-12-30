51927743a2726268858d4d852fb31001ef6f82fd
Michael Lehn <michael.lehn@uni-ulm.de>
2025-12-30 14:59:42 +0100

    rainbow colors

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -11,6 +11,25 @@ require('nvim-treesitter')
     })
     :wait(300000)
 
+vim.g.rainbow_delimiters = {
+    strategy = {
+        [''] = require('rainbow-delimiters').strategy['global'],
+    },
+    query = {
+        [''] = 'rainbow-delimiters',
+    },
+    highlight = {
+        'TSRainbowRed',
+        'TSRainbowYellow',
+        'TSRainbowOrange',
+        'TSRainbowCoral',
+        'TSRainbowViolet',
+        'TSRainbowBlue',
+        'TSRainbowPink',
+        'TSRainbowGreen',
+    },
+}
+
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'c', 'cpp', 'lua', 'python' },
     callback = function()
