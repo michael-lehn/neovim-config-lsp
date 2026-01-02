fd5b1eec927a7cc4d9f8b392f156160de76a91b2
Michael Lehn <michael.lehn@uni-ulm.de>
2026-01-02 18:03:57 +0100

    lean

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -60,6 +60,28 @@ require('lazy').setup({
                 })
             end,
         },
+        {
+            'Julian/lean.nvim',
+            event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+
+                -- optional dependencies:
+
+                -- a completion engine
+                --    hrsh7th/nvim-cmp or Saghen/blink.cmp are popular choices
+
+                -- 'nvim-telescope/telescope.nvim', -- for 2 Lean-specific pickers
+                -- 'andymass/vim-matchup',          -- for enhanced % motion behavior
+                -- 'andrewradev/switch.vim',        -- for switch support
+                -- 'tomtom/tcomment_vim',           -- for commenting
+            },
+
+            opts = { -- see below for full configuration options
+                mappings = true,
+            },
+        },
         -- ------------------------------------------------------------
         -- Colorscheme
         -- ------------------------------------------------------------
