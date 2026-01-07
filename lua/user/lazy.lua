438cead9144ae78dc38585668e906596eb94dbcd
Michael Lehn <michael.lehn@uni-ulm.de>
2026-01-07 01:05:41 +0100

    tex support

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -109,7 +109,10 @@ require('lazy').setup({
                 if sys == 'Darwin' then
                     vim.g.vimtex_view_method = 'skim'
                 elseif sys == 'Linux' then
-                    vim.g.vimtex_view_method = 'okular'
+                    vim.g.vimtex_view_method = 'general'
+                    vim.g.vimtex_view_general_viewer = 'okular'
+                    vim.g.vimtex_view_general_options =
+                        '--unique file:@pdf\\#src:@line@tex'
                 else
                     vim.g.vimtex_view_method = 'general'
                     vim.g.vimtex_view_general_viewer = 'open'
