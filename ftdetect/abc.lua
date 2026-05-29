b5e15f0b62dc7423fd40b46432a9d144103c0c13
Michael Lehn <michael.lehn@uni-ulm.de>
2026-05-29 14:41:09 +0200

    detect abc header

--- ftdetect/abc.lua
+++ ftdetect/abc.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = "*.abc",
-	callback = function()
-		vim.bo.filetype = "abc"
-	end,
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    pattern = { '*.abc', '*.hdr' },
+    callback = function()
+        vim.bo.filetype = 'abc'
+    end,
 })
